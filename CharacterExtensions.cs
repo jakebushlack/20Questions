c02e8a1563dddc6bc5e2dfc9cec8e3747d7248a2
jakebushlack <jakebushlack@gmail.com>
2022-08-02 13:51:42 -0600

    Initial project and template

---
+++ CharacterExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace TwentyQuestionsWebApp;
+
+public static class CharacterExtentions
+{
+    public static bool IsVowel(this char c)
+    {
+        long x = (long)(char.ToUpper(c)) - 64;
+        
+        if (x*x*x*x*x - 51*x*x*x*x + 914*x*x*x - 6894*x*x + 20205*x - 14175 == 0) return true;
+        
+        return false;
+    }
+}
